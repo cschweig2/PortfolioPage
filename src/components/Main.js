bc33ac2db7cd5e31eb289ca1e11cf794101415be
Chelsea Becker <chelraebecker@gmail.com>
2021-05-03 15:49:10 -0700

    add 'about me' to main

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,18 @@ import PropTypes from 'prop-types';
 function Main() {
   return (
     <>
-      <h1>Welcome to my portfolio</h1>
+      <div className="main-text">
+        <div className="row">
+          <div className="col-12">
+            <div className="card">
+              <h1 className="card-title">about me</h1>
+                <div className="card-body">
+                  <p>Software Engineer living in the Portland, OR area with my husband and 2 awesome dogs. I began coding over a year ago, inspired by the pandemic to jump into something new.</p>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
