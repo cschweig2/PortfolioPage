de74d5e06392394612a2cf43f39994bc6f9eeed9
Chelsea Becker <chelraebecker@gmail.com>
2021-05-03 15:48:54 -0700

    add navbar to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,20 @@ import React from "react";
 function Header() {
   return (
     <>
-      <h1 id="header">Chelsea Becker</h1>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Chelsea Becker</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="/resume">Resume</a>
+            <a class="nav-item nav-link" href="/projects">Projects</a>
+            <a class="nav-item nav-link" href="/contact">Contact Me</a>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
