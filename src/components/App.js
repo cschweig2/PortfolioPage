9870828666b63538d77b178a1f503671e0c951d4
Chelsea Becker <chelraebecker@gmail.com>
2021-05-04 15:35:25 -0700

    add blog component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,32 @@
 import './../App.css';
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Header from './Header';
 import Control from './Control';
+import Resume from './Resume';
+import ContactForm from './ContactForm';
+import Projects from './Projects';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <section id="main-background">
+      {/* <section id="main-background">
         <Control />
-      </section>
+      </section> */}
+      {/* <Control /> */}
+      <Switch>
+        <Route path='/resume' component={Resume}/>
+        <Route path='/contact' component={ContactForm}/>
+        <Route path='/projects' component={Projects}/>
+        <Route path='/home' component={Main}/>
+      </Switch>
+      <Footer />
     </div>
   );
 }
+// doesn't route immediately back to home 
 
 export default App;
