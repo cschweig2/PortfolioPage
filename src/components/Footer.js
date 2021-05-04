7ac6bb056c32ec55f03242955c2cd03276bc583a
Chelsea Becker <chelraebecker@gmail.com>
2021-05-04 15:30:37 -0700

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+        <div className="container footer">
+            <p id='footer-text'><strong>Chelsea Becker 2021</strong></p>
+        </div>
+    </>
+  )
+}
+
+export default Footer;
