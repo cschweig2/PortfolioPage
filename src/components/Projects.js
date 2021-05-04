a77d325da7c9d4778ea44ef75a2bac98eab9f963
Chelsea Becker <chelraebecker@gmail.com>
2021-05-04 15:30:52 -0700

    add projects component

---
+++ src/components/Projects.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// carousel of projects
+function Projects() {
+  return (
+    <>
+        <h1 id="component-header-text">Projects</h1>
+        <div className="carousel-item">
+            <img src="..." alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+                <h5>...</h5>
+                <p>...</p>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Projects;
