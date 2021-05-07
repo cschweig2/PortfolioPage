7aa5ff891beb0d3344363f862432e0f5c4da3a59
Chelsea Becker <chelraebecker@gmail.com>
2021-05-06 17:41:46 -0700

    temp remove control

--- src/components/Control.js
+++ src/components/Control.js
@@ -1,72 +1,74 @@
-import React from 'react';
-import Resume from './Resume';
-import Blog from './Blog';
-import ContactForm from './ContactForm';
-import Main from './Main';
+// import React from 'react';
+// import Resume from './Resume';
+// import Blog from './Blog';
+// import ContactForm from './ContactForm';
+// import Main from './Main';
 
-class Control extends React.Component {
+// class Control extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      formVisibleOnPage: false,
-      resumeVisibleOnPage: false,
-      blogVisibleOnPage: false
-    };
-  }
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       formVisibleOnPage: false,
+//       resumeVisibleOnPage: false,
+//       blogVisibleOnPage: false,
+//       projectsVisibleOnPage: false
+//     };
+//   }
 
-  handleSendingMessage = (contactInfoToAdd) => {
-    // send message to my email?
-    this.setState({ formVisibleOnPage: false });
-  }
+//   handleSendingMessage = (contactInfoToAdd) => {
+//     // send message to my email?
+//     this.setState({ formVisibleOnPage: false });
+//   }
 
-  handleClick = () => {
-    if (this.state.resumeVisibleOnPage) {
-      this.setState({
-        resumeVisibleOnPage: false
-      });
-    } else if (this.state.blogVisibleOnPage) {
-      this.setState({
-        blogVisibleOnPage: false
-      });
-    }
-  }
+//   handleClick = () => {
+//     if (this.state.resumeVisibleOnPage) {
+//       this.setState({
+//         resumeVisibleOnPage: false
+//       });
+//     } else if (this.state.blogVisibleOnPage) {
+//       this.setState({
+//         blogVisibleOnPage: false
+//       });
+//     }
+//   }
 
-  handleEditClick = () => {
-    this.setState({ editing: true });
-  }
+//   handleEditClick = () => {
+//     this.setState({ editing: true });
+//   }
 
-  render() {
-    let currentVisibleState = null;
-    let buttonText = null;
-    if (this.state.formVisibleOnPage)  {
-      currentVisibleState = <ContactForm onSendMessage={this.handleSendingMessage} />
-      buttonText='Return to Home';
-    } else if (this.state.blogVisibleOnPage) {
-      currentVisibleState = <Blog />
-      buttonText='Return to Home';
-    } else if (this.state.resumeVisibleOnPage) {
-      currentVisibleState = <Resume />
-      buttonText='Return to Home';
-    } else {
-      currentVisibleState = <Main />
-    }
-    if (!this.state.formVisibleOnPage && !this.state.blogVisibleOnPage && !this.state.resumeVisibleOnPage) {
-      return (
-        <>
-          {currentVisibleState}
-        </>
-      );
-    } else {
-      return (
-        <>
-          {currentVisibleState}
-          <button onClick={this.handleClick}>{buttonText}</button>
-        </>
-      );
-    }
-  }
+//   componentDidMount = () => {
+//     if (window.location.href === 'http://localhost:3000/contact') {
+//       this.setState({ formVisibleOnPage: true })
+//     } else if (window.location.href === 'http://localhost:3000/resume') {
+//       this.setState({ resumeVisibleOnPage: true })
+//     } else if (window.location.href === 'http://localhost:3000/projects') {
+//       this.setState({ projectsVisibleOnPage: true })
+//     } else {
+//       this.setState({ 
+//         formVisibleOnPage: false,
+//         resumeVisibleOnPage: false,
+//         projectsVisibleOnPage: false })
+//     }
+//   }
 
-}
+//   render() {
+//     let currentVisibleState = null;
+//     if (this.state.formVisibleOnPage)  {
+//       currentVisibleState = <ContactForm onSendMessage={this.handleSendingMessage} />
+//     } else if (this.state.blogVisibleOnPage) {
+//       currentVisibleState = <Blog />
+//     } else if (this.state.resumeVisibleOnPage) {
+//       currentVisibleState = <Resume />
+//     } else {
+//       currentVisibleState = <Main />
+//     }
+//     return (
+//         <>
+//           {currentVisibleState}
+//         </>
+//       );
+//   }
+// }
 
-export default Control;
+// export default Control;
