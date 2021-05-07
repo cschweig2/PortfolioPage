9f876235296bbce075fd929e5a44366bf159bea0
Chelsea Becker <chelraebecker@gmail.com>
2021-05-06 17:41:20 -0700

    modify entry fields, add new funcs to pass user input

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,78 +1,125 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios'
+import React from 'react'
 
-function ContactForm(props) {
-  return (
-    <>
-      <h1 id="component-header-text">Contact Me</h1>
-      <div className="card contact-form">
-        <div className="card-body">
-          <form className="row g-3 needs-validation" novalidate onSubmit={handleSendingMessageToEmail}>
-            <div className="col-md-6">
-              <input
-                type='text'
-                name='firstName'
-                placeholder='First Name' 
-                className='form-control'
-                required />
-            </div>
-            <div className="col-md-6">
-              <input
-                type='text'
-                name='lastName'
-                placeholder='Last Name' 
-                className='form-control'
-                required />
-            </div>
-            <div className="col-md-6">
-              <input
-                type='text'
-                name='email'
-                placeholder='Email' 
-                className="form-control"
-                required />
-            </div>
-            <div className="col-md-6">
-              <input
-                type='text'
-                name='phone'
-                placeholder='Phone' 
-                className="form-control"
-                required />
-            </div>
-            <div className="col-md-12">
-              <input
-                type='textbox'
-                name='message'
-                placeholder='Message'
-                className='form-control'
-                required />
-            </div>
-            <br />
-            <br />
-            <div className="col-12">
-              <button className='btn btn-info' type='submit'>Contact Me</button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </>
-  );
+class ContactForm extends React.Component {
 
-  function handleSendingMessageToEmail(event) {
-    event.PreventDefault();
-    props.onSendMessage({
-      firstName: event.target.firstName.value,
-      lastName: event.target.lastName.value,
-      email: event.target.email.value,
-      phone: event.target.phone.value,
-      message: event.target.message.value
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      message: ''
+    };
+  };
+
+  onFirstNameChange(event) {
+    this.setState({firstName: event.target.value})
+  };
+
+  onLastNameChange(event) {
+    this.setState({lastName: event.target.value})
+  };
+
+  onEmailChange(event) {
+    this.setState({email: event.target. value})
+  };
+
+  onPhoneChange(event) {
+    this.setState({phone: event.target. value})
+  };
+
+  onMessageChange(event) {
+    this.setState({message: event.target. value})
+  };
+
+  submitEmail(e){
+    e.preventDefault();
+    axios({
+      method: 'POST',
+      url: 'http://localhost:3001/contact',
+      data: this.state
+    }).then((response) => {
+      if (response.status === 'success') {
+        alert('message sent!');
+        this.resetForm();
+      } else {
+        alert('message failed!');
+      }
     });
-  }
-}
+  };
 
-ContactForm.propTypes = {
-  onSendMessage: PropTypes.func
+  resetForm() {
+    this.setState({firstName: '', lastName: '', email: '', phone: '', message: ''});
+  };
+
+  render() {
+    return (
+      <>
+        <h1 id="component-header-text">Contact Me</h1>
+        <div className="card contact-form">
+          <div className="card-body">
+            <form className="row g-3 needs-validation" id='contact-form' onSubmit={this.submitEmail.bind(this)} noValidate method='post' action='/contact' encType='multipart/form-data'>
+              <div className="col-md-6">
+                <input
+                  type='text'
+                  name='firstName'
+                  placeholder='First Name' 
+                  className='form-control'
+                  onChange={this.onFirstNameChange.bind(this)}
+                  value={this.state.firstName}
+                  required />
+              </div>
+              <div className="col-md-6">
+                <input
+                  type='text'
+                  name='lastName'
+                  placeholder='Last Name' 
+                  className='form-control'
+                  onChange={this.onLastNameChange.bind(this)}
+                  value={this.state.lastName}
+                  required />
+              </div>
+              <div className="col-md-6">
+                <input
+                  type='text'
+                  name='email'
+                  placeholder='Email' 
+                  className="form-control"
+                  onChange={this.onEmailChange.bind(this)}
+                  value={this.state.email}
+                  required />
+              </div>
+              <div className="col-md-6">
+                <input
+                  type='text'
+                  name='phone'
+                  placeholder='Phone' 
+                  className="form-control"
+                  onChange={this.onPhoneChange.bind(this)}
+                  value={this.state.phone}
+                  required />
+              </div>
+              <div className="col-md-12">
+                <textarea
+                  type='text'
+                  name='message'
+                  placeholder='Message'
+                  className='form-control'
+                  onChange={this.onMessageChange.bind(this)}
+                  value={this.state.message}
+                  required />
+              </div>
+              <div className="col-12">
+                <button className='btn btn-info' type='submit'>Contact Me</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </>
+    );
+  };
 };
 
 export default ContactForm;
